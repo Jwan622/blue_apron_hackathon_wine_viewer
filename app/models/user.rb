c12edcf5a3cfc53f019e2d5b6180705dbd7c0412
Jeffrey Wan <jeffrey.wan@blueapron.com>
2017-01-13 15:31:23 -0500

    remove geocoder hooks for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ActiveRecord::Base
   has_many :users_wines
   has_many :wines, through: :users_wines
-
-  geocoded_by :address   # can also be an IP address
-  after_validation :geocode
 end
