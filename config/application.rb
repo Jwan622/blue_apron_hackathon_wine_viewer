f97f8c261a76e1776b242b1a1181e5e2bcf99126
Jeffrey Wan <jeffrey.wan@blueapron.com>
2017-01-13 13:11:57 -0500

    fix rack cors request config

--- config/application.rb
+++ config/application.rb
@@ -31,12 +31,12 @@ module WineViewer
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-  end
-end
 
-use Rack::Cors do
-  allow do
-    origins '*'
-    resource '*', headers: :any, methods: :any
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
